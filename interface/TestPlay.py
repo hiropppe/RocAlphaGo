e7b983509fdc607cd709a094278f675e07aea466
root <root@docker-host.1.0.0.11>
2016-11-17 08:50:27 +0900

    Some fix

--- interface/TestPlay.py
+++ interface/TestPlay.py
@@ -1,7 +1,15 @@
 """Interface for AlphaGo self-play"""
+import numpy as np
+
 from AlphaGo.go import GameState
 
+
+WHITE = -1
+BLACK = +1
+EMPTY = 0
+
 AXIS = 'abcdefghijklmnopqrs'
+RESULT = 'DBW'
 
 
 class play_match(object):
@@ -14,6 +22,7 @@ class play_match(object):
         self.state = GameState(size=size)
         self.current = self.player1
         self.opponent = self.player2
+        self.playout = False
         # I Propose that GameState should take a top-level save directory,
         # then automatically generate the specific file name
 
@@ -24,41 +33,58 @@ class play_match(object):
         # self.state.write_to_disk()
         if len(self.state.history) > 1:
             if self.state.history[-1] is None and self.state.history[-2] is None \
-                    and self.state.current_player == -1:
+                    and self.state.current_player == WHITE:
                 end_of_game = True
             else:
                 end_of_game = False
         else:
             end_of_game = False
-        if not end_of_game:
+
+        if end_of_game:
+            self.playout = True
+        else:
             self.current, self.opponent = self.opponent, self.current
-        return end_of_game
-    
-    def play(self, showboard=True):
-        """ Play by current player """
-        end_of_game = self._play()
-        if showboard:
-            self.showboard()
+
         return end_of_game
 
-    def playeach(self, turn=1, showboard=True):
-        """Play one turn, update game state, save to disk"""
-        for i in xrange(turn*2):
-            end_of_game = self._play()
-            if end_of_game:
-                break
+    def clear(self, showboard=True):
+        self.state = GameState(size=self.state.size)
+        self.playout = False
+
         if showboard:
             self.showboard()
-        return end_of_game
+
+    def play(self, showboard=True):
+        """ Play by current player """
+        if not self.playout:
+            self._play()
+
+        if showboard:
+            self.showboard()
+
+        return self.playout
+
+    def playover(self, turn=300, showboard=True):
+        """Play one turn, update game state, save to disk"""
+        if not self.playout:
+            for i in xrange(turn*2):
+                self._play()
+                if self.playout:
+                    break
+
+        if showboard:
+            self.showboard()
+
+        return self.playout
 
     def showboard(self):
         """
            a b c d e f g
          a . . . . . . . a     ;W(bb)
-         b . o . . . . . b
-         c . . . B . . . c
+         b . W . . . . . b
+         c . . . x . . . c
          d . . . . . . . d
-         e . . W . B . . e
+         e . . o . x . . e
          f . . . . . . . f
          g . . . . . . . g
            a b c d e f g
@@ -73,15 +99,19 @@ class play_match(object):
                 elif j in (0, self.state.size + 1):
                     ch = AXIS[i-1]
                 elif self.state.board[j-1][i-1] == 1:
-                    if self.state.history[-1][0] == j-1 and self.state.history[-1][1] == i-1:
-                        ch = 'x'
-                    else:
+                    if self.state.history[-1] \
+                       and self.state.history[-1][0] == j-1 \
+                       and self.state.history[-1][1] == i-1:
                         ch = 'B'
-                elif self.state.board[j-1][i-1] == -1:
-                    if self.state.history[-1][0] == j-1 and self.state.history[-1][1] == i-1:
-                        ch = 'o'
                     else:
+                        ch = 'x'
+                elif self.state.board[j-1][i-1] == -1:
+                    if self.state.history[-1] \
+                       and self.state.history[-1][0] == j-1 \
+                       and self.state.history[-1][1] == i-1:
                         ch = 'W'
+                    else:
+                        ch = 'o'
                 elif self.state.size == 19 and (i-1 in (3, 9, 15)) and (j-1 in (3, 9, 15)):
                     ch = '+'
                 else:
@@ -91,10 +121,35 @@ class play_match(object):
                 board.append(' ')
 
             if i == 1 and self.state.history:
-                board.append('    ;{}({}{})'.format('W' if self.state.current_player == 1 else 'B',
-                                                    AXIS[self.state.history[-1][0]],
-                                                    AXIS[self.state.history[-1][1]]))
+                if self.state.history[-1]:
+                    board.append('    ;{}({}{})'.format('W' if self.state.current_player == 1 and not self.playout else 'B',
+                                                        AXIS[self.state.history[-1][0]],
+                                                        AXIS[self.state.history[-1][1]]))
+                else:
+                    board.append('    ;{}(tt)'.format('W' if self.state.current_player == 1 and not self.playout else 'B'))
+
+            if i == 3 and self.playout:
+                score_white, score_black = self.calculate_score()
+                board.append('    ' + ('Draw' if not self.state.get_winner() else 'Winner: {}'.format(RESULT[self.state.get_winner()])))
+                board.append(' (W: {}, B: {})'.format(score_white, score_black))
 
             board.append('\n')
 
         print(''.join(board))
+
+    def calculate_score(self):
+        """Calculate score of board state and return player ID (1, -1, or 0 for tie)"""
+        # Count number of positions filled by each player, plus 1 for each eye-ish space owned
+        score_white = np.sum(self.state.board == WHITE)
+        score_black = np.sum(self.state.board == BLACK)
+        empties = zip(*np.where(self.state.board == EMPTY))
+        for empty in empties:
+            # Check that all surrounding points are of one color
+            if self.state.is_eyeish(empty, BLACK):
+                score_black += 1
+            elif self.state.is_eyeish(empty, WHITE):
+                score_white += 1
+        score_white += self.state.komi
+        score_white -= self.state.passes_white
+        score_black -= self.state.passes_black
+        return score_white, score_black
