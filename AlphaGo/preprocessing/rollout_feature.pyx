137db6b92f0936a08e35a897627925e94700661a
hiropppe <sergei.0910@gmail.com>
2017-01-24 19:35:36 +0900

    Testing cython impl

--- AlphaGo/preprocessing/rollout_feature.pyx
+++ AlphaGo/preprocessing/rollout_feature.pyx
@@ -1,5 +1,11 @@
 import numpy as np
 
+cimport numpy as np
+
+DTYPE = np.int
+
+ctypedef np.int_t DTYPE_t
+
 
 class RolloutFeature(object):
     """
@@ -8,10 +14,19 @@ class RolloutFeature(object):
     __NEIGHBOR8_CACHE = {}
 
     def __init__(self, state, pat3x3_file=None, pat12d_file=None):
-        self.state = state
+        cdef int size
+        cdef int n_response
+        cdef int n_save_atari
+        cdef int n_neighbor
+        cdef int n_feature
+
+        cdef int n_3x3
+        cdef int n_12d
+
+        cdef np.ndarray feature
+
         self.size = state.size
 
-        n_position = self.size ** 2
         self.n_response = 1
         self.n_save_atari = 1
         self.n_neighbor = 8
@@ -33,8 +48,8 @@ class RolloutFeature(object):
             self.n_3x3 = 0
             self.n_12d = 0
 
-        n_feature = self.n_response + self.n_save_atari + self.n_neighbor + self.n_3x3 + self.n_12d
-        self.feature = np.zeros((n_position, n_feature))
+        self.n_feature = self.n_response + self.n_save_atari + self.n_neighbor + self.n_3x3 + self.n_12d
+        self.feature = np.zeros((self.size**2, self.n_feature), dtype=DTYPE)
 
         self.idx_response = (0, self.n_response)
         self.idx_save_atari = (self.idx_response[1], self.idx_response[1] + self.n_save_atari)
@@ -42,41 +57,41 @@ class RolloutFeature(object):
         self.idx_3x3 = (self.idx_neighbor[1], self.idx_neighbor[1] + self.n_3x3)
         self.idx_12d = (self.idx_3x3[1], self.idx_3x3[1] + self.n_12d)
 
-        self._create_neighbor8_cache()
+        self._create_neighbor8_cache(state)
         self._prev_neighbors = None
 
-    def _get_neighbor8(self, center):
+    def _get_neighbor8(self, state, center):
         (x, y) = center
         neighbors = [(x-1, y-1), (x-1, y), (x-1, y+1),
                      (x,   y-1),           (x,   y+1),
                      (x+1, y-1), (x+1, y), (x+1, y+1)]
         xy = np.array([[nx*self.size + ny, self.idx_neighbor[0]+i]
-                       for i, (nx, ny) in enumerate(neighbors) if self.state._on_board((nx, ny))])
+                       for i, (nx, ny) in enumerate(neighbors) if state._on_board((nx, ny))])
         return (xy[:, 0], xy[:, 1])
 
-    def _create_neighbor8_cache(self):
+    def _create_neighbor8_cache(self, state):
         for x in range(self.size):
             for y in range(self.size):
-                RolloutFeature.__NEIGHBOR8_CACHE[(x, y)] = self._get_neighbor8((x, y))
+                RolloutFeature.__NEIGHBOR8_CACHE[(x, y)] = self._get_neighbor8(state, (x, y))
 
-    def update(self):
-        if self.state.history:
-            prev_move = self.state.history[-1]
+    def update(self, state):
+        if state.history:
+            prev_move = state.history[-1]
         else:
             prev_move = None
 
         self.update_neighbors(prev_move)
-        self.update_save_atari()
+        self.update_save_atari(state)
 
-    def update_save_atari(self):
+    def update_save_atari(self, state):
         self.feature[:, self.idx_save_atari[0]] = 0
-        for (x, y) in self.state.last_liberty_cache[self.state.current_player]:
-            if len(self.state.liberty_sets[x][y]) > 1:
+        for (x, y) in state.last_liberty_cache[state.current_player]:
+            if len(state.liberty_sets[x][y]) > 1:
                 self.feature[x*self.size + y, self.idx_save_atari[0]] = 1
             else:
-                for (nx, ny) in self.state._neighbors((x, y)):
-                    if (self.state.board[nx, ny] == self.state.current_player
-                       and self.state.liberty_counts[nx, ny] > 1):
+                for (nx, ny) in state._neighbors((x, y)):
+                    if (state.board[nx, ny] == state.current_player
+                       and state.liberty_counts[nx, ny] > 1):
                         self.feature[x*self.size + y, self.idx_save_atari[0]] = 1
 
     def update_neighbors(self, prev_move):
@@ -87,11 +102,12 @@ class RolloutFeature(object):
         if prev_move:
             # set new neighbors
             (nx_ix, ny_ix) = RolloutFeature.__NEIGHBOR8_CACHE[prev_move]
+            print("{} {}".format(nx_ix, ny_ix))
             self.feature[nx_ix, ny_ix] = 1
             self._prev_neighbors = (nx_ix, ny_ix)
 
-    def get_tensor():
+    def get_tensor(self):
         pass
 
-    def get_sparse_tensor():
+    def get_sparse_tensor(self):
         pass
