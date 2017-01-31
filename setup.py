7723659b85f57f133f97f9971f1cb566de0c7523
hiropppe <sergei.0910@gmail.com>
2017-01-31 17:10:19 +0900

    Add gtp script

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+
 import numpy
 
-from distutils.core import setup
+import setuptools
+
+from distutils import core
 from Cython.Build import cythonize
 
-setup(
+core.setup(
   ext_modules=cythonize(
       ["AlphaGo/preprocessing/rollout_feature.pyx"],
       language="c++",
@@ -11,4 +15,41 @@ setup(
       extra_link_args=["-std=c++11"]
   ),
   include_dirs=[numpy.get_include()]
+)
+
+
+requires = [
+]
+
+
+setuptools.setup(
+  name='RocAlphaGo',
+  version='0.0.1',
+  author='take',
+  url='',
+  packages=[
+    'AlphaGo', 'AlphaGo.models', 'AlphaGo.preprocessing', 'AlphaGo.training', 'interface'
+  ],
+  scripts=[
+    'scripts/rag', 'scripts/ragc'
+  ],
+  install_requires=requires,
+  license='MIT',
+  test_suite='test',
+  classifiers=[
+    'Operating System :: OS Independent',
+    'Environment :: Console',
+    'Programming Language :: Python',
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'Topic :: Utilities',
+  ],
+  data_files=[
+    ('/usr/local/share/rocalphago/', ['model/policy.json']),
+    ('/usr/local/share/rocalphago/', ['model/policy.hdf5']),
+    ('/usr/local/share/rocalphago/', ['model/value.json']),
+    ('/usr/local/share/rocalphago/', ['model/value.hdf5']),
+  ]
 )
