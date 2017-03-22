3568d0188cbe39d7489e35a36b25c9e9373f0094
hiropppe <sergei.0910@gmail.com>
2017-03-22 14:15:43 +0900

    Update test Docker env

--- tools/docker/bootstrap.sh
+++ tools/docker/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir /mnt/s3_mount_name
-/root/go/bin/goofys -f s3_bucket_name /mnt/s3_mount_name
+mkdir /mnt/s3
+/root/go/bin/goofys -f s3_bucket_name /mnt/s3
--- tools/docker/run_worker_for_init.sh
+++ tools/docker/run_worker_for_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 /etc/init.d/rsyslog start
 
-mkdir /mnt/s3_mount_name
-/root/go/bin/goofys s3_bucket_name /mnt/s3_mount_name
+mkdir /mnt/s3
+/root/go/bin/goofys s3_bucket_name /mnt/s3
 
 cd /root/RocAlphaGo
-nohup python -u -m AlphaGo.training.reinforcement_policy_trainer_tensorflow --cluster_spec /cluster --job_name $(hostname | awk -F- '{print $1}') --task_index $(hostname | awk -F- '{print $2}') --logdir /mnt/s3/logs/rl_policy --opponent_pool /mnt/s3/opponents --sync True --keras_weights /mnt/s3/logs/policy.hdf5 --gpu_memory_fraction 0.2 > /root/worker.log 2>&1 &
+nohup python -u -m AlphaGo.training.reinforcement_policy_trainer_tensorflow --cluster_spec /cluster --job_name $(hostname | awk -F- '{print $1}') --task_index $(hostname | awk -F- '{print $2}') --logdir /mnt/s3/logs --sync True --keras_weights /mnt/s3/weights/policy.hdf5 --gpu_memory_fraction 0.2 > /root/worker.log 2>&1 &
 
 tail -f /dev/null
