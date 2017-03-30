fecaa768da18e578893f817649fd0dd79cae0d1f
hiropppe <sergei.0910@gmail.com>
2017-03-30 09:53:14 +0900

    Fix goofys cache_ttl

--- tools/docker/bootstrap.sh
+++ tools/docker/bootstrap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir /s3
-/root/go/bin/goofys -f --stat-cache-ttl 0 --type-cache-ttl 0 s3_bucket_name /s3
+/root/go/bin/goofys -f s3_bucket_name /s3
+# /root/go/bin/goofys -f --stat-cache-ttl 0 --type-cache-ttl 0 s3_bucket_name /s3
--- tools/docker/run_worker.sh
+++ tools/docker/run_worker.sh
@@ -2,7 +2,8 @@
 /etc/init.d/rsyslog start
 
 mkdir /s3
-/root/go/bin/goofys --stat-cache-ttl 0 --type-cache-ttl 0 s3_bucket_name /s3
+/root/go/bin/goofys s3_bucket_name /s3
+# /root/go/bin/goofys --stat-cache-ttl 0 --type-cache-ttl 0 s3_bucket_name /s3
 
 cd /root/RocAlphaGo
 nohup python -u -m AlphaGo.training.reinforcement_policy_trainer_tensorflow --cluster_spec /cluster --job_name $(hostname | awk -F- '{print $1}') --task_index $(hostname | awk -F- '{print $2}') --sharedir /s3 --summary_logdir /tmp/logs --sync True --gpu_memory_fraction 0.2 > worker.log 2>&1 &
